90b6d3b333a022e9951bf20722e483065eab2038
Ava <ava@example.com>
2020-11-18 18:09:56 -0500

    updated js

---
+++ scripts/menu.js
@@ -0,0 +1,15 @@
+
+{
+  $('#nav-icon').click(function() {
+    $('#nav-icon, #menu').toggleClass('open');
+  });
+
+  $('#menu a').click(function() {
+    $('#nav-icon, #menu').toggleClass('open');
+  });
+
+  // --- trying to get scroll snap consistent ---
+  setTimeout(function() {
+    $('#homepage').addClass('scroll-snap');
+  }, 2000);
+}
